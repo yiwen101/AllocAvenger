c691e773243cd4bc0027c3b5e90766085cf6159c
yiwen101 <e0968880@u.nus.edu>
2023-09-07 00:47:54 +0800

    delete unnecessary parsers, tested ad and mod parsers, add samplers into builders

--- raw_parsers/AdPropertiesParser.py
+++ raw_parsers/AdPropertiesParser.py
@@ -4,11 +4,15 @@ import ExcelParser as eParser
 class AdPropertiesParser:
     def __init__(self):
         self.baseParser = eParser.ExcelToPropertiesParser()
+        self.ans = None
+    # run from the project directory
+    def parse(self):
+        if self.ans is not None:
+            return self.ans
         
-    def parse(self, advertisementStr):
-        propertiess = self.baseParser.parse(advertisementStr)
+        propertiess = self.baseParser.parse("input/raw_data.xlsx","ads dimension (dim table)")
         ans = []
-        for properties in properties:
+        for properties in propertiess:
             # quite some ad_revenue are None, so we need to filter them out
             if properties["ad_revenue"] is None or properties["baseline_st"] is None:
                 continue
@@ -17,22 +21,12 @@ class AdPropertiesParser:
             if properties["avg_ad_revenue"] is None:
                 properties["avg_ad_revenue"] = 0
             ans.append(properties)
+        self.ans = ans
         return ans
             
                
         
 
-'''
-def adPropertiesParserTest():
-    parser = AdPropertiesParser()
-    testStr = "p_data//20230807::ad_id//1747578422390810::delivery_country//US::queue_market//US&CA::punish_num//1::avg_ad_revenue//4796.25::ad_revenue//513217::start_time//10/24/2022 14:36::baseline_st//1.78"
-    property = parser.parse(testStr)
-    ok = strPar.StrToPropertiesParser.propertyAssertHelper(
-        ["ad_id", "queue_market", "punish_num", "avg_ad_revenue", "ad_revenue",
-         "baseline_st"],
-        [1747578422390810, ["US", "CA"], 1, 4796.25, 513217, 1.78], property)
-    if ok:
-        print("adPropertiesParserTest passed")
-    else:
-        print("adPropertiesParserTest failed")
-'''
+#ads = AdPropertiesParser().parse()
+#print(ads[0])
+#print(ads[1])
--- raw_parsers/ModeratorPropertiesParser.py
+++ raw_parsers/ModeratorPropertiesParser.py
@@ -4,29 +4,20 @@ import ExcelParser as eParser
 class ModeratorPropertiesParser:
     def __init__(self):
         self.baseParser = eParser.ExcelToPropertiesParser()
-
+        self.ans = None
+    
     # make sure the data input is valid, entry with empty values are either rewritten as null value of the type or discarded.
-    def parse(self, path, sheet_name):
-        propertiess = self.baseParser.parse(path, sheet_name)
+    def parse(self):
+        if self.ans is not None:
+            return self.ans
+        propertiess = self.baseParser.parse("input/raw_data.xlsx","moderator dimension (dim table)")
         ans = []
         for properties in propertiess:
-            if properties["Productivity"] is None or properties["Utilisation"] is None or properties["handling time"] is None or properties["accuracy"] is None:
+            if not isinstance(properties["Productivity"], float) or not isinstance(properties["Utilisation %"], float) or not isinstance(properties["handling time"], int) or not isinstance(properties["accuracy"], float):
                 continue
-            ans.append(properties)   
+            ans.append(properties) 
+        self.ans = ans  
         return ans
 
-'''
-def moderatorPropertiesParserTest():
-    parser = ModeratorPropertiesParser()
-    testStr = "moderator//1689841547143170::market//SA&OM&BH&QA&JO::Productivity//286.2176::Utilisation//0.8124::handling time//123549::accuracy//0.99"
-    property = parser.parse(testStr)
-    ok = strPar.StrToPropertiesParser.propertyAssertHelper(
-        ["moderator", "market", "Productivity", "Utilisation", "handling time",
-         "accuracy"],
-        [1689841547143170, ["SA", "OM", "BH", "QA", "JO"], 286.2176, 0.8124,
-         123549, 0.99], property)
-    if ok:
-        print("moderatorPropertiesParserTest passed")
-    else:
-        print("moderatorPropertiesParserTest failed")
-'''
+#parser = ModeratorPropertiesParser()
+#print(parser.parse()[0]["accuracy"])
