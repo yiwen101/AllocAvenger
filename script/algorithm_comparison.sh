1cd2167b79032a854fd4eeefa73862dd95c157d6
Tim-Siu <e1088140@u.nus.edu>
2023-09-09 19:47:34 +0800

    Updated visualisation

--- script/algorithm_comparison.sh
+++ script/algorithm_comparison.sh
@@ -1,7 +1,7 @@
 SCRIPT="test.py"
 for algo in random greedy_idle greedy greedy_farseeing; do
-    FILENAME="result_even_1_1_synthetic_${algo}.json"
+    FILENAME="result_even_1_1_raw_${algo}.json"
     echo "Running with algorithm: $algo..."
-    python $SCRIPT -d even -v 1 -p 1 -s synthetic -a $algo -f $FILENAME
+    python $SCRIPT -d even -v 1 -p 1 -s raw -a $algo -f $FILENAME
 done
 echo "Algorithm comparison completed."
