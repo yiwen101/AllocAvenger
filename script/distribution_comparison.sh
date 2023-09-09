9f01a056bd2a3a38b33a08ba94a184f4acaac9f5
Tim-Siu <e1088140@u.nus.edu>
2023-09-09 20:58:23 +0800

    Updated visualisation

--- script/distribution_comparison.sh
+++ script/distribution_comparison.sh
@@ -1,7 +1,7 @@
 SCRIPT="test.py"
 for dist in even uneven; do
-    FILENAME="result_${dist}_1_1_synthetic_greedy_farseeing.json"
+    FILENAME="result_${dist}_1_1_raw_greedy.json"
     echo "Running for distribution: $dist..."
-    python $SCRIPT -d $dist -v 1 -p 1 -s synthetic -a greedy_farseeing -f $FILENAME
+    python $SCRIPT -d $dist -v 1 -p 1 -s raw -a greedy -f $FILENAME
 done
 echo "Distribution comparison completed."
